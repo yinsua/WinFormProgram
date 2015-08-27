3531cc42d18df94d6a050241bca001606654af63
yinsua <ysh.001@qq.com>
2015-08-27 12:53:23 +0800

    完善了一些细节，增加了注释。

--- GetWindowsHandler/Form1.cs
+++ GetWindowsHandler/Form1.cs
@@ -15,10 +15,10 @@ namespace GetWindowsHandler
 {
     public partial class Form1 : Form
     {
+        
+        
 
-        #region 变量声明及方法定义
-        IntPtr hwnd;   //窗口句柄
-
+        #region Dll Import 
         [DllImport("user32.DLL")]
         private static extern IntPtr SendMessage(int hWnd, uint Msg, int wParam, IntPtr lParam);
 
@@ -30,12 +30,10 @@ namespace GetWindowsHandler
         [DllImport("User32.dll")]
         public static extern int EnumChildWindows(IntPtr WinHandle, EnumChildWindow ecw, string name);
 
-        #endregion 
-
         public delegate bool EnumChildWindowsProc(IntPtr hwnd, uint lParam);
         public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
 
-        #region Dll Import 
+
 
         [DllImport("User32.dll", EntryPoint = "FindWindow")]
         private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
@@ -52,43 +50,10 @@ namespace GetWindowsHandler
         [DllImport("user32.dll", EntryPoint = "GetClassName")]
         public static extern int GetClassName(IntPtr hwnd, StringBuilder lpClassName, int nMaxCount);
         #endregion
-        private void SearchWindow()
-        {
-            #region
-            string lpszParentClass = "TMain"; //整个窗口的类名 
-            string lpszParentWindow = "Member's Information and Result."; //窗口标题 
-            IntPtr hWndParent = new IntPtr(0);
-            IntPtr EdithWnd = new IntPtr(0);
-
-            //查到窗体，得到整个窗体 
-            hWndParent = FindWindow(lpszParentClass, lpszParentWindow);
-
-            EnumChildWindowsProc myEnumChild = new EnumChildWindowsProc(EumWinChiPro);
-            try
-            {
-                EnumChildWindows(hWndParent, myEnumChild, 0);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message + "\r\n" + ex.Source + "\r\n\r\n" + ex.StackTrace.ToString());
-            }
-        }
-        #endregion
-
+        
         getContent content = new getContent();
-
-        public static int count;
-        public bool EumWinChiPro(IntPtr hWnd, uint lParam)
-        {
-            StringBuilder s = new StringBuilder(50);
-            GetClassName(hWnd, s, 50);
-            MessageBox.Show(s.ToString() + '\n' + content.GetControlText(hWnd));
-            count++;
-            return true;
-        }
-
-        public static string all = null;
-        public static int num = 0;
+        public static int count = 0;
+        private static string [] userInformationFromPanel = new string[19];
 
         public Form1()
         {
@@ -97,17 +62,59 @@ namespace GetWindowsHandler
         }
 
         private void button1_Click(object sender, EventArgs e)
-        {
-            content.RegisterControlforMessages();
+        {            
+            //开始遍历
             SearchWindow();
             this.button1.Text = count.ToString();
+        }
+
+        private void SearchWindow()
+        {
+            string lpszParentClass = "TMain"; //整个窗口的类名 
+            string lpszParentWindow = "Member's Information and Result."; //窗口标题 
+            IntPtr hWndParent = new IntPtr(0);
+            IntPtr EdithWnd = new IntPtr(0);
+
+            //查到窗体，得到整个窗体 
+            hWndParent = FindWindow(lpszParentClass, lpszParentWindow);
+
+            //实例化
+            EnumChildWindowsProc myEnumChild = new EnumChildWindowsProc(EumWinChiPro);
+            try
+            {
+                //遍历子控件
+                EnumChildWindows(hWndParent, myEnumChild, 0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\r\n" + ex.Source + "\r\n\r\n" + ex.StackTrace.ToString());
+            }
+        }
+
+        //遍历每个子空间的具体方法
+        public bool EumWinChiPro(IntPtr hWnd, uint lParam)
+        {
+            StringBuilder s = new StringBuilder(64);
+            //根据子窗口句柄获取窗口类名
+            GetClassName(hWnd, s, 64);
+            //根据子窗口句柄获取其内容并弹窗显示
+            MessageBox.Show(count.ToString() + '\n' + content.GetControlText(hWnd));
+            //把获取到的信息放到指定字符串
+            if (count > 5 && count < 10)
+            {
+                userInformationFromPanel[count] = content.GetControlText(hWnd);
+            }
+            count++;            
+            return true;
         }
 
     }
 
 
+    //根据窗体句柄获取其内容的类
     public class getContent
     {
+        #region DLL import
         [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
         public static extern int RegisterWindowMessage(string lpString);
 
@@ -115,14 +122,18 @@ namespace GetWindowsHandler
         public static extern bool SendMessage(IntPtr hWnd, uint Msg, int wParam, StringBuilder lParam);
         [DllImport("user32.dll", SetLastError = true)]
         public static extern IntPtr SendMessage(int hWnd, int Msg, int wparam,int lparam);
+        #endregion
 
         const int WM_GETTEXT = 0x000D;
         const int WM_GETTEXTLENGTH = 0x000E;
 
+        //注册消息
         public void RegisterControlforMessages()
         {
             RegisterWindowMessage("WM_GETTEXT");
         }
+
+        //根据句柄获取其内容
         public string GetControlText(IntPtr hWnd)
         {
 
@@ -135,7 +146,7 @@ namespace GetWindowsHandler
             if (size > 0)
             {
                 title = new StringBuilder(size + 1);
-
+                //通过消息来设置或得到属性（内容、事件等）
                 SendMessage(hWnd, (int)WM_GETTEXT, title.Capacity, title);                
             }
             return title.ToString();
